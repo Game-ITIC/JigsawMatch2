3f212d95bf6a0fe9c4695f92255013aaad20c0ee
HikikamoriKo <88240116+WORlDTron@users.noreply.github.com>
2024-08-27 17:33:02 +0500

    UpdateNotification

--- Assets/JuiceFresh/Scripts/Notification.cs
+++ Assets/JuiceFresh/Scripts/Notification.cs
@@ -17,38 +17,17 @@ public class Notification : MonoBehaviour
 
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
 
-        // Запускаем отправку уведомлений через заданные интервалы времени
-        ScheduleDailyNotifications();
+        // Отправляем уведомление через 5 секунд после запуска игры
+        SendTestNotification();
     }
 
-    private void ScheduleDailyNotifications()
+    private void SendTestNotification()
     {
-        // Удаляем все предыдущие уведомления, чтобы избежать дублирования
-        AndroidNotificationCenter.CancelAllScheduledNotifications();
-
-        // Устанавливаем уведомление на первую половину дня
-        SendNotificationAtTime(10, 0); // 10:00 AM
-
-        // Устанавливаем уведомление на вторую половину дня
-        SendNotificationAtTime(18, 0); // 6:00 PM
-    }
-
-    private void SendNotificationAtTime(int hour, int minute)
-    {
-        System.DateTime now = System.DateTime.Now;
-        System.DateTime fireTime = new System.DateTime(now.Year, now.Month, now.Day, hour, minute, 0);
-
-        // Если текущее время уже прошло, устанавливаем уведомление на следующий день
-        if (fireTime < now)
-        {
-            fireTime = fireTime.AddDays(1);
-        }
-
         AndroidNotification notification = new AndroidNotification()
         {
             Title = "Butterflies are waiting for you",
             Text = "Discover new parts of the park and improve the old ones, make your park even better!",
-            FireTime = fireTime,
+            FireTime = System.DateTime.Now.AddSeconds(5),
             SmallIcon = "small_icon",
         };
 
