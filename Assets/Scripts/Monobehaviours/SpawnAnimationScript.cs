fe91ce133b6a2285d978d7b319e05d015a0cca1c
ankalimel <stephanie.bakunz@gmail.com>
2025-04-08 16:08:18 +0500

    New fonts

---
+++ Assets/Scripts/Monobehaviours/SpawnAnimationScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAnimationScript : MonoBehaviour
+{
+    [SerializeField] private float scaleDuration, jumpDuration, landDuration;
+    public void Animate()
+    {
+        
+    }
+}
