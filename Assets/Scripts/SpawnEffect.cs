23c2eb0f94763cec7432e29d62e9f5767a54bcdf
ankalimel <stephanie.bakunz@gmail.com>
2025-04-18 17:40:42 +0500

    Spawn animation

---
+++ Assets/Scripts/SpawnEffect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpawnEffect : MonoBehaviour
+{
+    private float animationDuration = 0.5f;
+    private float jumpPower = 2f;
+    private float overshootScale = 1.5f;
+
+
+    void Start()
+    {
+        PlaySpawnAnimation();
+    }
+
+    void PlaySpawnAnimation()
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 startScale = originalScale * 0.1f;
+        Vector3 bigScale = originalScale * overshootScale;
+        Vector3 startPosition = transform.position;
+
+        transform.localScale = startScale;
+
+        Sequence spawnSequence = DOTween.Sequence();
+        spawnSequence.Append(transform.DOJump(startPosition, jumpPower, 1, animationDuration));
+        spawnSequence.Join(transform.DOScale(bigScale, animationDuration));
+        spawnSequence.Append(transform.DOScale(originalScale, animationDuration));
+
+        //sound
+        GetComponent<AudioSource>().Play();
+    }
+}
