0b079404b26f34f822cce896c45c113bd4039d69
ankalimel <stephanie.bakunz@gmail.com>
2025-04-18 18:34:53 +0500

    HideUnhide btn

---
+++ Assets/Scripts/HideUnhideScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideUnhideScript : MonoBehaviour
+{
+    [SerializeField] private Button eyeButton;
+    [SerializeField] private GameObject eyeHide, eyeUnhide;
+    [SerializeField] private GameObject uiElement1, uiElement2, uiElement3, uiElement4;
+    private bool isVisible = true;
+
+    private void Start()
+    {
+        eyeButton.onClick.AddListener(OnEyeButtonClick);
+    }
+    private void OnEyeButtonClick()
+    {
+        if (isVisible == true)
+        {
+            uiElement1.SetActive(false);
+            uiElement2.SetActive(false);
+            uiElement3.SetActive(false);
+            uiElement4.SetActive(false);
+            isVisible = false;
+            eyeHide.SetActive(false);
+            eyeUnhide.SetActive(true);
+        }
+        else
+        {
+            uiElement1.SetActive(true);
+            uiElement2.SetActive(true);
+            uiElement3.SetActive(true);
+            uiElement4.SetActive(true);
+            isVisible = true;
+            eyeHide.SetActive(true);
+            eyeUnhide.SetActive(false);
+        }
+        
+    }
+}
