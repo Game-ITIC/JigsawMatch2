e1de57683771b4ebc2c5b011987231f836c793f5
Sardor Akhmadjonov <sardorakh7@gmail.com>
2025-05-02 12:51:35 +0500

    Update: Shop opener

--- Assets/Scripts/Views/InAppView.cs
+++ Assets/Scripts/Views/InAppView.cs
@@ -1,10 +1,12 @@
+using Interfaces;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Views
 {
-    public class InAppView : MonoBehaviour
+    public class InAppView : MonoBehaviour, IPreload
     {
+        [SerializeField] private Button closeButton;
         [SerializeField] private Button noAdsButton;
         [SerializeField] private Button gemsButton;
         [SerializeField] private Button coinsButton;
@@ -14,5 +16,21 @@ namespace Views
         public Button NoAdsButton => noAdsButton;
         public Button GemsButton => gemsButton;
         public Button CoinsButton => coinsButton;
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Warmup()
+        {
+            closeButton.onClick.RemoveAllListeners();
+            closeButton.onClick.AddListener(Hide);
+        }
     }
 }
