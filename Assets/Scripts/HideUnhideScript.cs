906a3c54cb7749b9a3528f8fd5a5c32623e7516e
ankalimel <stephanie.bakunz@gmail.com>
2025-05-02 14:14:37 +0500

    Egypt Island

--- Assets/Scripts/HideUnhideScript.cs
+++ Assets/Scripts/HideUnhideScript.cs
@@ -7,7 +7,7 @@ public class HideUnhideScript : MonoBehaviour
 {
     [SerializeField] private Button eyeButton;
     [SerializeField] private GameObject eyeHide, eyeUnhide;
-    [SerializeField] private GameObject uiElement1, uiElement2, uiElement3, uiElement4;
+    [SerializeField] private GameObject uiElement1, uiElement2, uiElement3, uiElement4, uiElement5;
     private bool isVisible = true;
 
     private void Start()
@@ -22,6 +22,7 @@ public class HideUnhideScript : MonoBehaviour
             uiElement2.SetActive(false);
             uiElement3.SetActive(false);
             uiElement4.SetActive(false);
+            uiElement5.SetActive(false);
             isVisible = false;
             eyeHide.SetActive(false);
             eyeUnhide.SetActive(true);
@@ -32,6 +33,7 @@ public class HideUnhideScript : MonoBehaviour
             uiElement2.SetActive(true);
             uiElement3.SetActive(true);
             uiElement4.SetActive(true);
+            uiElement5.SetActive(true);
             isVisible = true;
             eyeHide.SetActive(true);
             eyeUnhide.SetActive(false);
