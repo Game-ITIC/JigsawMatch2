b4355e5675bfc1904b017b576acb42e84bc20a57
Green-Blood <bloodjey98@gmail.com>
2025-07-23 16:37:28 +0500

    Fix: boosters

--- Assets/JuiceFresh/Scripts/GUI/BoostShop.cs
+++ Assets/JuiceFresh/Scripts/GUI/BoostShop.cs
@@ -8,7 +8,7 @@ using Unity.VisualScripting;
 public enum BoostType
 {
     Shovel,
-    Energy, 
+    Energy,
     Bomb,
     ExtraMoves
 }
@@ -24,10 +24,10 @@ public class BoostShop : MonoBehaviour
     BoostType boostType;
 
     public List<BoostProduct> boostProducts = new List<BoostProduct>();
-    
-    public void SetBoost(BoostType _boostType)
+
+    public void SetBoost(BoostType boostType)
     {
-        boostType = _boostType;
+        this.boostType = boostType;
         gameObject.SetActive(true);
         //icon.sprite = boostProducts[(int)_boostType].icon;
         //description.text = boostProducts[(int)_boostType].description;
@@ -36,25 +36,27 @@ public class BoostShop : MonoBehaviour
         //    transform.Find("Image/BuyBoost" + (i + 1) + "/Count").GetComponent<Text>().text = "x" + boostProducts[(int)_boostType].count[i];
         //    transform.Find("Image/BuyBoost" + (i + 1) + "/Price").GetComponent<Text>().text = "" + boostProducts[(int)_boostType].GemPrices[i];
         //}
-        var boostProduct = boostProducts[(int)_boostType];
+        var boostProduct = boostProducts[(int)boostType];
         icon.sprite = boostProduct.icon;
         description.text = boostProduct.description;
-        var countTextTransform = transform.Find("Image/BuyBoost1/Count");
-        var priceTextTransform = transform.Find("Image/BuyBoost1/Price");
+        var countTextTransform = transform.Find("Image/Buttons/BuyButton/Count");
+        var priceTextTransform = transform.Find("Image/Buttons/BuyButton/Price");
 
-        if (countTextTransform != null)
+        if(countTextTransform != null)
         {
             var countText = countTextTransform.GetComponent<Text>();
-            if (countText != null)
+
+            if(countText != null)
             {
                 countText.text = "x" + boostProduct.count[0]; // Access the first element
             }
         }
 
-        if (priceTextTransform != null)
+        if(priceTextTransform != null)
         {
             var priceText = priceTextTransform.GetComponent<Text>();
-            if (priceText != null)
+
+            if(priceText != null)
             {
                 priceText.text = "" + boostProduct.GemPrices[0]; // Access the first element
             }
@@ -63,17 +65,17 @@ public class BoostShop : MonoBehaviour
 
     public void BuyBoost(GameObject button)
     {
-        int count = int.Parse(button.transform.Find("Count").GetComponent<Text>().text.Replace("x", ""));
-        int price = int.Parse(button.transform.Find("Price").GetComponent<Text>().text);
-        
-        GetComponent<AnimationManager>().BuyBoost(boostType, price, count, boostProducts[(int)boostType].icon);
+        // int count = int.Parse(button.transform.Find("Count").GetComponent<Text>().text.Replace("x", ""));
+        int price = int.Parse(button.transform.Find("Image/Buttons/BuyButton/Price").GetComponent<Text>().text);
+
+        GetComponent<AnimationManager>().BuyBoost(boostType, price, 1, boostProducts[(int)boostType].icon);
     }
 
     public void WatchAd()
     {
         var boostProduct = boostProducts[(int)boostType];
         icon.sprite = boostProduct.icon;
-        
+
         LevelManager.Instance.AdRewardService.SetAdRewardType(AdRewardType.Booster, boostType);
         LevelManager.Instance.AdRewardService.SetInfo(boostProduct.icon, $"You got {boostType}");
         LevelManager.Instance.IronSourceManager.ShowRewardedAd();
