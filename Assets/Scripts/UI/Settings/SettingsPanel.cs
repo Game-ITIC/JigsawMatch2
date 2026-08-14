b49e5b670a450d104aacd360e643c5bc455e8676
Laziz Abduhalimov <lazizabduhalimov35@gmail.com>
2026-08-14 12:21:39 +0500

    Update: settings panel

--- Assets/Scripts/UI/Settings/SettingsPanel.cs
+++ Assets/Scripts/UI/Settings/SettingsPanel.cs
@@ -1,9 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SettingsPanel : MonoBehaviour
 {
+    [SerializeField] private TMP_Text tMP_Text;
     [SerializeField] private Button[] _closeButtons;
-    [SerializeField] private SettingsToggleView _settingsToggleViewPrefab;
-    [SerializeField] private Transform _settingsToggleViewParent;
+    [SerializeField] private SettingsToggleView _soundToggleView;
+    [SerializeField] private SettingsToggleView _musicToggleView;
+    [SerializeField] private SettingsToggleView _vibrationToggleView;
+    [SerializeField] private SettingsToggleView _notificationToggleView;
+    // [SerializeField] private Transform _settingsToggleViewParent;
 }
--- Assets/Scripts/UI/Settings/SettingsToggleView.cs
+++ Assets/Scripts/UI/Settings/SettingsToggleView.cs
@@ -6,4 +6,5 @@ public class SettingsToggleView : MonoBehaviour
 {
     [SerializeField] private Image _image;
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private CustomToggle _toggle;
 }
